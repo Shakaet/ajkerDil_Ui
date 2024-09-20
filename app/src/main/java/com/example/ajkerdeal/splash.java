5e7660cd67685e648c3df1a3317d20c588ee7b6e
Shakaet <abdshakaet@gmail.com>
2024-09-20 15:59:09 +0600

    add

---
+++ app/src/main/java/com/example/ajkerdeal/splash.java
@@ -0,0 +1,34 @@
+package com.example.ajkerdeal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class splash extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT=1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent ii = new Intent(splash.this, MainActivity.class);
+
+                startActivity(ii);
+                finish();
+            }
+        },SPLASH_TIME_OUT );
+
+    }
+}
